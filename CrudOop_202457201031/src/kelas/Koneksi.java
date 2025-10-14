672f33283aa51758948b176c17b299a195ebaff5
ubaydillah <m.ubaiydillah.r@gmail.com>
2025-10-14 18:12:30 +0700

    commit2

--- CrudOop_202457201031/src/kelas/Koneksi.java
+++ CrudOop_202457201031/src/kelas/Koneksi.java
@@ -1,9 +1,31 @@
 package kelas;
 
-/**
- *
- * @author asus
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Koneksi {
     
+    private static Connection mysqlconfig;
+    
+    public static Connection konek() {
+        
+        try {
+            String url = "jdbc:mysql://localhost:3306/kaylaparfume";
+            
+            //Username database
+            String user = "root";
+            
+            //password database
+            String pass = "";
+            
+            mysqlconfig = DriverManager.getConnection(url, user, pass);
+        } catch (SQLException e) {
+            //Menampilkan pesan error jika koneksi gagal
+            System.err.println(e.getMessage());
+        }
+        
+        //Mengembalikan objek koneksi (bisa null jika gagal)
+        return mysqlconfig;    
+    }   
 }
